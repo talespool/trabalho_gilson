a7984cc5bf4a89790463e191595ff5deed444879
talespool <tales.pimentel@edu.unifor.br>
2024-11-30 17:58:50 -0300

    uhul

---
+++ src/main/java/com/example/aslkjaoibacjk/HelloController.java
@@ -0,0 +1,14 @@
+package com.example.aslkjaoibacjk;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example.aslkjaoibacjk {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.example.aslkjaoibacjk to javafx.fxml;
+    exports com.example.aslkjaoibacjk;
+}
